dbe6f4e805adad10d527cb34f0534a34c90be832
jacynraquel <jacynr@escochecks.com>
2025-06-30 16:35:40 +0800

    Initial commit with home page

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import {Link, NavLink} from 'react-router-dom';
+export default function Footer(){
+	return(
+		<Container className="bg-primary">
+			<Row>
+				{/*Column 1*/}
+				<Col>
+				 <Link href="#">
+				 <img src="#" alt="logo"/>
+				 </Link>
+
+				 <h4>My Portfolio</h4>
+				 <p>Lorem ipsum</p>
+				</Col>
+
+				<Col>
+				 <Link href="#">
+				 <img src="#" alt="logo"/>
+				 </Link>
+
+				 <h4>My Portfolio</h4>
+				 <p>Lorem ipsum</p>
+				</Col>
+
+				<Col>
+				 <Link href="#">
+				 <img src="#" alt="logo"/>
+				 </Link>
+
+				 <h4>My Portfolio</h4>
+				 <p>Lorem ipsum</p>
+				</Col>
+
+				<Col>
+				 <Link href="#">
+				 <img src="#" alt="logo"/>
+				 </Link>
+
+				 <h4>My Portfolio</h4>
+				 <p>Lorem ipsum</p>
+				</Col>
+			</Row>
+		</Container>	
+		)
+}
---
+++ src/components/HeroSection.js
@@ -0,0 +1,17 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import {useState, useEffect} from 'react';
+
+export default function HeroSection(){
+
+	const [title, setTitle] = useState()
+	return(
+		<Container>
+			<Row>
+				<Col>
+					<h1> Hi, I'm Jacyn, a Web Developer!</h1>
+					<h3> I build fast, modern, and responsive websites </h3>
+				</Col>
+			</Row>
+		</Container>
+		)
+}
